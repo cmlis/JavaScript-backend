4099c11052e96cbe5a9632e181e1c40dc4914a6a
Camila Santana <cssantana8@gmail.com>
2022-05-16 15:17:42 -0600

    Exemplos Aula 01 - Null

---
+++ nullAndUnde.js
@@ -0,0 +1,15 @@
+
+
+let input = null; //null
+let input2; //undefined
+
+if (input == null) {
+
+    console.log("there is no information available")
+}else{
+    console.log(input);
+}
+
+console.log("Outro")
+console.log(null == undefined); // true
+console.log(null === undefined); // false
